49d2e32af399ef65fba424fcec0c53d099cffc26
Aaron Griffith <aargri@gmail.com>
2019-09-20 18:02:42 -0400

    let stream.sh take an argument

--- stream.sh
+++ stream.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-source stream-credentials.env
+# see stream-credentials.env.example for the type of file to use here
+source "$@"
 
 python3 radio.py radio -o "ffmpegre:-acodec libmp3lame -ab 300k -content_type audio/mpeg -f mp3 icecast://source:$STREAM_PW@$STREAM_HOST/$STREAM_MOUNT" -m "$STREAM_SCHEMA://source:$STREAM_PW@$STREAM_HOST/admin/metadata?mount=%2F$STREAM_MOUNT&mode=updinfo" $STREAM_FILES
