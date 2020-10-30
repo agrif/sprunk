a8553d0a1a7549a8ed0f6f2b3e108bdf269d987b
Aaron Griffith <aargri@gmail.com>
2020-10-29 23:24:49 -0400

    change main to accept media file from argv

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 use sprunk::Sink;
 
 fn main() -> anyhow::Result<()> {
-    //let file = std::fs::File::open(r"H:\local\gtav-radio\RADIO_02_POP\tape_loop\TAPE_LOOP.ogg")?;
-    //let file = std::fs::File::open(r"H:\local\gtav-radio\RADIO_02_POP\six_underground\SIX_UNDERGROUND.ogg")?;
-    let file = std::fs::File::open(r"C:\Users\agrif\Desktop\dance_6.ogg")?;
-
+    let args: Vec<String> = std::env::args().collect();
+    let file = std::fs::File::open(&args[1])?;
     let source = sprunk::source::Media::new(file)?;
-    //let source = sprunk::source::Sine::new(44100.0, 1, 440.0);
-
     let mut sink = sprunk::sink::System::new(1024)?;
     sink.play(source, 1024)?;
 
